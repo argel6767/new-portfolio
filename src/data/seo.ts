18882185257b79b5ac5a4f499990aa93a82adc2d
Argel Hernandez <argel6767@gmail.com>
2025-07-12 10:04:14 -0500

    add keywords for se0.js

--- src/data/seo.ts
+++ src/data/seo.ts
@@ -3,25 +3,31 @@ const SEO ={
     home:{
         title: "Argel's Personal Portfolio",
         description: "Argel Hernandez Amaya's Portfolio",
-        keywords: ["React", "Next.js", "Argel Hernandez Amaya", "Argel Hernandez", "Argel", "Portfolio"],
+        keywords: ["React", "Next.js", "Argel Hernandez Amaya", "Argel Hernandez", "Argel", "Portfolio", "Java", "Spring Boot", "Quarkus", "JavaFX",
+        "TypeScript", "JavaScript", "Vue.js", "Node.js", "Python", "Django", "Docker", "Azure", "Azure App Service", "Azure SQL", "Azure Storage", "CI-CD"],
     },
 
     about:{
         title: "About | Argel's Portfolio",
         description: "Contact Argel Hernandez Amaya",
-        keywords: ["React", "Next.js", "Argel Hernandez Amaya", "Argel Hernandez", "Argel", "Portfolio", "About", "about"],
+        keywords: ["React", "Next.js", "Argel Hernandez Amaya", "Argel Hernandez", "Argel", "Portfolio", "About", "about", "Work History", "Work history", "Education", "education",
+        "experience", "Experience", "Internship", "Intern", "Software Engineer", "Backend Engineer", "Software Developer", "Backend Developer"],
     },
 
     projects: {
         title: "Projects | Argel's Portfolio",
         description: "Contact Argel Hernandez Amaya",
-        keywords: ["React", "Next.js", "Argel Hernandez Amaya", "Argel Hernandez", "Argel", "Portfolio", "projects", "Projects", "project"],
+        keywords: ["React", "Next.js", "Argel Hernandez Amaya", "Argel Hernandez", "Argel", "Portfolio", "projects", "Projects", "project", "Java", "Spring Boot", "Quarkus", "JavaFX",
+            "TypeScript", "JavaScript", "Vue.js", "Node.js", "Python", "Django", "Skills", "skills", "Technologies", "technologies", "Tools", "tools", "Languages", "languages",
+            "Docker", "Azure", "Azure App Service", "Azure SQL", "Azure Storage", "CI-CD", "MongoDB", "Scrum", "Agile", "GitHub", "GitHub Actions", "GitHub Pages", "GitHub Actions", "Git",
+        "Microservices"],
     },
 
     contact: {
         title: "Contact | Argel's Portfolio",
         description: "Contact Argel Hernandez Amaya",
-        keywords: ["React", "Next.js", "Argel Hernandez Amaya", "Argel Hernandez", "Argel", "Portfolio", "contact", "Contact", "Reach"],
+        keywords: ["React", "Next.js", "Argel Hernandez Amaya", "Argel Hernandez", "Argel", "Portfolio", "contact", "Contact", "Reach",
+        "Email", "email", "LinkedIn", "GitHub"],
     }
 }
 
